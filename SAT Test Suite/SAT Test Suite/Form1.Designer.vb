7f2f29f75ff77345af674aebef9e351d38ef5fcc
Arun Kumar <arun.kumar@smartantennatech.com>
2016-05-27 13:55:40 +0100

    Started creating SAT Test Suite for plotting and comparing S parameters, efficiency etc.
    Signed-off-by: Arun Kumar <arun.kumar@smartantennatech.com>

---
+++ SAT Test Suite/SAT Test Suite/Form1.Designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits MetroFramework.Forms.MetroForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Dim ChartArea8 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend8 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series8 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Me.button1 = New MetroFramework.Controls.MetroButton()
+        Me.pictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.label3 = New System.Windows.Forms.Label()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button2 = New MetroFramework.Controls.MetroButton()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.pictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'button1
+        '
+        Me.button1.DisplayFocus = True
+        Me.button1.FontWeight = MetroFramework.MetroButtonWeight.Regular
+        Me.button1.Location = New System.Drawing.Point(803, 145)
+        Me.button1.Name = "button1"
+        Me.button1.Size = New System.Drawing.Size(119, 38)
+        Me.button1.TabIndex = 0
+        Me.button1.Text = "Start"
+        Me.button1.UseSelectable = True
+        '
+        'pictureBox1
+        '
+        Me.pictureBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.pictureBox1.Image = CType(resources.GetObject("pictureBox1.Image"), System.Drawing.Image)
+        Me.pictureBox1.Location = New System.Drawing.Point(783, 453)
+        Me.pictureBox1.Name = "pictureBox1"
+        Me.pictureBox1.Size = New System.Drawing.Size(176, 101)
+        Me.pictureBox1.TabIndex = 15
+        Me.pictureBox1.TabStop = False
+        '
+        'label3
+        '
+        Me.label3.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.label3.Font = New System.Drawing.Font("Calibri", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, CType(0, Byte))
+        Me.label3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(4, Byte), Integer), CType(CType(37, Byte), Integer), CType(CType(84, Byte), Integer))
+        Me.label3.Location = New System.Drawing.Point(731, 542)
+        Me.label3.Name = "label3"
+        Me.label3.Size = New System.Drawing.Size(46, 15)
+        Me.label3.TabIndex = 16
+        Me.label3.Text = "Ver. 1.0"
+        '
+        'Chart1
+        '
+        ChartArea8.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea8)
+        Legend8.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend8)
+        Me.Chart1.Location = New System.Drawing.Point(39, 128)
+        Me.Chart1.Name = "Chart1"
+        Series8.ChartArea = "ChartArea1"
+        Series8.Legend = "Legend1"
+        Series8.Name = "Series1"
+        Me.Chart1.Series.Add(Series8)
+        Me.Chart1.Size = New System.Drawing.Size(709, 411)
+        Me.Chart1.TabIndex = 17
+        Me.Chart1.Text = "Chart1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(748, 210)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 13)
+        Me.Label1.TabIndex = 18
+        Me.Label1.Text = "Internal (ms):"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(821, 208)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(101, 20)
+        Me.NumericUpDown1.TabIndex = 19
+        '
+        'Timer1
+        '
+        '
+        'Button2
+        '
+        Me.Button2.DisplayFocus = True
+        Me.Button2.FontWeight = MetroFramework.MetroButtonWeight.Regular
+        Me.Button2.Location = New System.Drawing.Point(803, 101)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(119, 38)
+        Me.Button2.TabIndex = 20
+        Me.Button2.Text = "Select"
+        Me.Button2.UseSelectable = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.SystemColors.Window
+        Me.MenuStrip1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(20, 60)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(942, 24)
+        Me.MenuStrip1.TabIndex = 21
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.OenToolStripMenuItem, Me.ToolStripMenuItem1, Me.SaveToolStripMenuItem, Me.ToolStripMenuItem2, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.NewToolStripMenuItem.Text = "New"
+        '
+        'OenToolStripMenuItem
+        '
+        Me.OenToolStripMenuItem.Name = "OenToolStripMenuItem"
+        Me.OenToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.OenToolStripMenuItem.Text = "Open"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(149, 6)
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.SaveToolStripMenuItem.Text = "Save"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(149, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'Form1
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.BackMaxSize = 30
+        Me.ClientSize = New System.Drawing.Size(982, 577)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Chart1)
+        Me.Controls.Add(Me.label3)
+        Me.Controls.Add(Me.pictureBox1)
+        Me.Controls.Add(Me.button1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.ShadowType = MetroFramework.Forms.MetroFormShadowType.AeroShadow
+        Me.Text = "SAT Test Suite"
+        CType(Me.pictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents button1 As MetroFramework.Controls.MetroButton
+    Private WithEvents pictureBox1 As PictureBox
+    Private WithEvents label3 As Label
+    Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+    Friend WithEvents Label1 As Label
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button2 As MetroFramework.Controls.MetroButton
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents OenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripSeparator
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ SAT Test Suite/SAT Test Suite/Form1.vb
@@ -0,0 +1,75 @@
+﻿Imports MetroFramework.Controls
+Imports System.Reflection
+Imports Excel = Microsoft.Office.Interop.Excel
+
+Public Class Form1
+    'Define your Excel Objects
+    Dim xlApp As New Excel.Application
+    Dim xlWorkBook As Excel.Workbook
+    Dim x As Integer = 0
+    Dim array(100) As Integer
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        NumericUpDown1.Minimum = 1
+        NumericUpDown1.Maximum = 100
+        Chart1.Series.Clear()
+        Chart1.Series.Add("A")
+        Chart1.ChartAreas("ChartArea1").AxisX.Minimum = 0
+        Chart1.ChartAreas("ChartArea1").AxisX.Maximum = 100
+        Chart1.ChartAreas("ChartArea1").AxisX.Interval = 10
+        Chart1.ChartAreas("ChartArea1").AxisX.Title = "Frequency in Hz"
+        Chart1.ChartAreas("ChartArea1").AxisY.Minimum = -100
+        Chart1.ChartAreas("ChartArea1").AxisY.Maximum = 100
+        Chart1.ChartAreas("ChartArea1").AxisY.Interval = 20
+        Chart1.ChartAreas("ChartArea1").AxisY.Title = "y"
+        Chart1.Series("A").ChartType = DataVisualization.Charting.SeriesChartType.Line
+        For i As Integer = 0 To 100
+            array(i) = 0
+            Chart1.Series("A").Points.AddXY(i, array(i))
+        Next
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles button1.Click
+        If button1.Text = "Start" Then
+            button1.Text = "Stop"
+            Timer1.Interval = NumericUpDown1.Value
+            Timer1.Start()
+        Else
+            button1.Text = "Start"
+            Timer1.Stop()
+        End If
+        ''~~> Add a New Workbook
+        'xlWorkBook = xlApp.Workbooks.Add
+
+        ''~~> Display Excel
+        'xlApp.Visible = True
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        x = x + 1
+        For i As Integer = 0 To 99
+            array(i) = array(i + 1)
+        Next
+        array(100) = 100 * (Math.Sin(0.1 * x))
+        Chart1.Series("A").Points.Clear()
+        For i As Integer = 0 To 100
+            Chart1.Series("A").Points.AddXY(i, array(i))
+        Next
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NumericUpDown1.ValueChanged
+        Timer1.Interval = NumericUpDown1.Value
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim dialog As FolderBrowserDialog = New FolderBrowserDialog()
+        dialog.Description = "Select a spreadsheet to plot the values"
+        dialog.ShowDialog(Me)
+        If dialog.SelectedPath <> Nothing Then
+            '~~> Opens an exisiting Workbook. Change path and filename as applicable
+            xlWorkBook = xlApp.Workbooks.Open("C:\Tutorial\Sample.xlsx")
+
+            '~~> Display Excel
+            xlApp.Visible = True
+        End If
+    End Sub
+End Class
