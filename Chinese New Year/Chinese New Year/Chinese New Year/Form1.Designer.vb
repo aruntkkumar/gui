dba6da937ce90d9fd47cbba9816429acaf0d36b5
Arun Kumar <arun.kumar@smartantennatech.com>
2018-02-21 09:25:07 +0000

    Lucky Draw software created for Chinese New Year celebration...
    Signed-off-by: Arun Kumar <arun.kumar@smartantennatech.com>

---
+++ Chinese New Year/Chinese New Year/Chinese New Year/Form1.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits MetroFramework.Forms.MetroForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MetroButton1 = New MetroFramework.Controls.MetroButton()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.MetroLabel1 = New MetroFramework.Controls.MetroLabel()
+        Me.MetroLabel2 = New MetroFramework.Controls.MetroLabel()
+        Me.MetroLabel3 = New MetroFramework.Controls.MetroLabel()
+        Me.MetroLabel4 = New MetroFramework.Controls.MetroLabel()
+        Me.MetroLabel5 = New MetroFramework.Controls.MetroLabel()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MetroButton1
+        '
+        Me.MetroButton1.BackColor = System.Drawing.Color.Transparent
+        Me.MetroButton1.BackgroundImage = Global.Chinese_New_Year.My.Resources.Resources.Untitled
+        Me.MetroButton1.FontSize = MetroFramework.MetroButtonSize.Tall
+        Me.MetroButton1.ForeColor = System.Drawing.Color.Red
+        Me.MetroButton1.Location = New System.Drawing.Point(657, 379)
+        Me.MetroButton1.Name = "MetroButton1"
+        Me.MetroButton1.Size = New System.Drawing.Size(126, 68)
+        Me.MetroButton1.Style = MetroFramework.MetroColorStyle.Red
+        Me.MetroButton1.TabIndex = 0
+        Me.MetroButton1.Text = "PRESS HERE!!!"
+        Me.MetroButton1.Theme = MetroFramework.MetroThemeStyle.Light
+        Me.MetroButton1.UseCustomBackColor = True
+        Me.MetroButton1.UseCustomForeColor = True
+        Me.MetroButton1.UseSelectable = True
+        Me.MetroButton1.UseStyleColors = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.ErrorImage = CType(resources.GetObject("PictureBox1.ErrorImage"), System.Drawing.Image)
+        Me.PictureBox1.ImageLocation = "https://gfycat.com/gifs/detail/silkyhastyblackfly"
+        Me.PictureBox1.InitialImage = CType(resources.GetObject("PictureBox1.InitialImage"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(974, 379)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(459, 338)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox2.ErrorImage = CType(resources.GetObject("PictureBox2.ErrorImage"), System.Drawing.Image)
+        Me.PictureBox2.ImageLocation = "https://gfycat.com/gifs/detail/illustriouscookedanemonecrab"
+        Me.PictureBox2.InitialImage = CType(resources.GetObject("PictureBox2.InitialImage"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(3, 379)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(459, 338)
+        Me.PictureBox2.TabIndex = 2
+        Me.PictureBox2.TabStop = False
+        '
+        'MetroLabel1
+        '
+        Me.MetroLabel1.AutoSize = True
+        Me.MetroLabel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(65, Byte), Integer))
+        Me.MetroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall
+        Me.MetroLabel1.FontWeight = MetroFramework.MetroLabelWeight.Bold
+        Me.MetroLabel1.ForeColor = System.Drawing.Color.White
+        Me.MetroLabel1.Location = New System.Drawing.Point(574, 252)
+        Me.MetroLabel1.Name = "MetroLabel1"
+        Me.MetroLabel1.Size = New System.Drawing.Size(0, 0)
+        Me.MetroLabel1.TabIndex = 3
+        Me.MetroLabel1.UseCustomBackColor = True
+        Me.MetroLabel1.UseCustomForeColor = True
+        '
+        'MetroLabel2
+        '
+        Me.MetroLabel2.AutoSize = True
+        Me.MetroLabel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(65, Byte), Integer))
+        Me.MetroLabel2.FontSize = MetroFramework.MetroLabelSize.Tall
+        Me.MetroLabel2.FontWeight = MetroFramework.MetroLabelWeight.Bold
+        Me.MetroLabel2.ForeColor = System.Drawing.Color.Orange
+        Me.MetroLabel2.Location = New System.Drawing.Point(25, 91)
+        Me.MetroLabel2.Name = "MetroLabel2"
+        Me.MetroLabel2.Size = New System.Drawing.Size(157, 25)
+        Me.MetroLabel2.TabIndex = 4
+        Me.MetroLabel2.Text = "1st Prize: Orange"
+        Me.MetroLabel2.UseCustomBackColor = True
+        Me.MetroLabel2.UseCustomForeColor = True
+        '
+        'MetroLabel3
+        '
+        Me.MetroLabel3.AutoSize = True
+        Me.MetroLabel3.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(65, Byte), Integer))
+        Me.MetroLabel3.FontSize = MetroFramework.MetroLabelSize.Tall
+        Me.MetroLabel3.FontWeight = MetroFramework.MetroLabelWeight.Bold
+        Me.MetroLabel3.ForeColor = System.Drawing.Color.Yellow
+        Me.MetroLabel3.Location = New System.Drawing.Point(25, 125)
+        Me.MetroLabel3.Name = "MetroLabel3"
+        Me.MetroLabel3.Size = New System.Drawing.Size(157, 25)
+        Me.MetroLabel3.TabIndex = 5
+        Me.MetroLabel3.Text = "2nd Prize: Yellow"
+        Me.MetroLabel3.UseCustomBackColor = True
+        Me.MetroLabel3.UseCustomForeColor = True
+        '
+        'MetroLabel4
+        '
+        Me.MetroLabel4.AutoSize = True
+        Me.MetroLabel4.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(65, Byte), Integer))
+        Me.MetroLabel4.FontSize = MetroFramework.MetroLabelSize.Tall
+        Me.MetroLabel4.FontWeight = MetroFramework.MetroLabelWeight.Bold
+        Me.MetroLabel4.ForeColor = System.Drawing.Color.Pink
+        Me.MetroLabel4.Location = New System.Drawing.Point(25, 159)
+        Me.MetroLabel4.Name = "MetroLabel4"
+        Me.MetroLabel4.Size = New System.Drawing.Size(134, 25)
+        Me.MetroLabel4.TabIndex = 6
+        Me.MetroLabel4.Text = "3rd Prize: Pink"
+        Me.MetroLabel4.UseCustomBackColor = True
+        Me.MetroLabel4.UseCustomForeColor = True
+        '
+        'MetroLabel5
+        '
+        Me.MetroLabel5.AutoSize = True
+        Me.MetroLabel5.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(65, Byte), Integer))
+        Me.MetroLabel5.FontSize = MetroFramework.MetroLabelSize.Tall
+        Me.MetroLabel5.FontWeight = MetroFramework.MetroLabelWeight.Bold
+        Me.MetroLabel5.ForeColor = System.Drawing.Color.LimeGreen
+        Me.MetroLabel5.Location = New System.Drawing.Point(25, 193)
+        Me.MetroLabel5.Name = "MetroLabel5"
+        Me.MetroLabel5.Size = New System.Drawing.Size(255, 25)
+        Me.MetroLabel5.TabIndex = 7
+        Me.MetroLabel5.Text = "Complementary Prize: Green"
+        Me.MetroLabel5.UseCustomBackColor = True
+        Me.MetroLabel5.UseCustomForeColor = True
+        '
+        'Form1
+        '
+        Me.ApplyImageInvert = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.BackImage = Global.Chinese_New_Year.My.Resources.Resources.chinese_new_year_2018
+        Me.BackMaxSize = 1440
+        Me.ClientSize = New System.Drawing.Size(1440, 720)
+        Me.Controls.Add(Me.MetroLabel5)
+        Me.Controls.Add(Me.MetroLabel4)
+        Me.Controls.Add(Me.MetroLabel3)
+        Me.Controls.Add(Me.MetroLabel2)
+        Me.Controls.Add(Me.MetroLabel1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MetroButton1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Resizable = False
+        Me.Text = "SAT Chinese New Year Celebration"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MetroButton1 As MetroFramework.Controls.MetroButton
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents MetroLabel1 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroLabel2 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroLabel3 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroLabel4 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroLabel5 As MetroFramework.Controls.MetroLabel
+End Class
---
+++ Chinese New Year/Chinese New Year/Chinese New Year/Form1.vb
@@ -0,0 +1,89 @@
+﻿Public Class Form1
+
+    Dim rand As New Random
+    Dim nextperson As Integer = 1
+    Dim n() As Integer = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    Dim i As Integer = 0
+    Dim flag As Boolean = False
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MetroLabel1.Text = "Please select the next contestant..."
+    End Sub
+
+    Private Sub MetroButton1_Click(sender As Object, e As EventArgs) Handles MetroButton1.Click
+        If i = 16 Then
+            MetroLabel1.Text = "All the contestants have been selected..."
+            Exit Sub
+        End If
+        flag = False
+        While (flag = False)
+            nextperson = rand.Next(1, 17)
+            If Not (n.Contains(nextperson)) Then
+                n(i) = nextperson
+                Select Case nextperson
+                    Case 1
+                        MetroLabel1.Text = "Next contestant: Colin Tucker"
+                        i = i + 1
+                    Case 2
+                        MetroLabel1.Text = "Next contestant: Dr. Sampson Hu"
+                        i = i + 1
+                    Case 3
+                        MetroLabel1.Text = "Next contestant: Paul Moon"
+                        i = i + 1
+                    'Case 4
+                    '    MetroLabel1.Text = "Next contestant: Richard Cohen"
+                    '    i = i + 1
+                    Case 4
+                        MetroLabel1.Text = "Next contestant: David Tanner"
+                        i = i + 1
+                    Case 5
+                        MetroLabel1.Text = "Next contestant: David Beet"
+                        i = i + 1
+                    Case 6
+                        MetroLabel1.Text = "Next contestant: Ian Ormerod"
+                        i = i + 1
+                    Case 7
+                        MetroLabel1.Text = "Next contestant: Marzena Braminska"
+                        i = i + 1
+                    Case 8
+                        MetroLabel1.Text = "Next contestant: Jiaping Lu"
+                        i = i + 1
+                    Case 9
+                        MetroLabel1.Text = "Next contestant: Liang Wan"
+                        i = i + 1
+                    Case 10
+                        MetroLabel1.Text = "Next contestant: Qing Liu"
+                        i = i + 1
+                    Case 11
+                        MetroLabel1.Text = "Next contestant: Jiechen Chen"
+                        i = i + 1
+                    Case 12
+                        MetroLabel1.Text = "Next contestant: Thanh Nga Mai"
+                        i = i + 1
+                    Case 13
+                        MetroLabel1.Text = "Next contestant: Jinsong Song"
+                        i = i + 1
+                    Case 14
+                        MetroLabel1.Text = "Next contestant: Arun Kumar"
+                        i = i + 1
+                    Case 15
+                        MetroLabel1.Text = "Next contestant: Joanna Zhou"
+                        i = i + 1
+                    'Case 16
+                    '    MetroLabel1.Text = "Next contestant: Edwin Candy"
+                    '    i = i + 1
+                    Case 16
+                        MetroLabel1.Text = "Next contestant: Peter Hall"
+                        i = i + 1
+                        'Case 19
+                        '    MetroLabel1.Text = "Next contestant: Peter Gardner"
+                        '    i = i + 1
+                    Case Else
+                        MetroLabel1.Text = "Try again please..."
+                End Select
+                flag = True
+            End If
+        End While
+    End Sub
+
+End Class
